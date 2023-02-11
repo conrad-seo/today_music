790afd6c6bb7c5147993c2c617643b926a4b8657
zs <rewin6455@gmail.com>
2023-02-11 21:54:40 +0900

    입력값 에러메시지 추가

--- app.py
+++ app.py
@@ -28,13 +28,21 @@ def list_post():
     url_receive = request.form['url_give']
     url = url_receive
 
+    if url == '' or 'https' not in url or url.upper() == url.lower():
+        return jsonify({'msg': '유효한 주소가 아닙니다.'})
+
+    if star_receive =='':
+        return jsonify({'msg': '별점을 선택해주세요!'})
+
+    if comment_receive == '':
+        return jsonify({'msg': '코멘트를 작성해주세요!'})
+
+
     data = requests.get(url, headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
 
     title = soup.select_one('meta[property="og:title"]')['content']
     image = soup.select_one('meta[property="og:image"]')['content']
-
-
 
     doc = {
         'star': star_receive,
@@ -44,8 +52,22 @@ def list_post():
         'image' : image
     }
     db.list.insert_one(doc)
+    return jsonify ({'msg':'등록 완료!'})
 
-    return jsonify({'title': title})
+
+    # if url == '' or 'https' not in url or url.upper() == url.lower():
+
+    #
+    #
+    # if id_receive == '' or email_receive == '' or password_receive == '' or pwcf_receive == '':
+    #     return jsonify({'ans': 'fail', 'msg': '공백이 있습니다'})
+    # elif '@' not in email_receive or '.' not in email_receive:
+    #     return jsonify({'ans': 'fail', 'msg': '이메일 형식이 아닙니다.'})
+    # elif pwcf_receive != password_receive:
+    #     return jsonify({'ans': 'fail', 'msg': '비밀번호가 다릅니다'})
+
+
+    # return jsonify({'msg': '등록 완료!','title': title})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
